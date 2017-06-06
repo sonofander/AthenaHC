22b31e34a6e043506379ada4321741735d77f9a6
DylanKaye <dylankaye210@gmail.com>
2017-06-06 15:15:19 -0500

    Add files via upload

---
+++ athena.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import numpy as np
+from nltk import word_tokenize, pos_tag
+from nltk.corpus import wordnet as wn
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+df = pd.read_csv('...',header=None)
+words = df.values[:,2]
+
+def similarity(s1,s2):
+
+    def penn_to_wn(tag):
+        """ Convert between a Penn Treebank tag to a simplified Wordnet tag """
+        if tag.startswith('N'):
+            return 'n'
+
+        if tag.startswith('V'):
+            return 'v'
+
+        if tag.startswith('J'):
+            return 'a'
+
+        if tag.startswith('R'):
+            return 'r'
+
+        return None
+
+    def tagged_to_synset(word, tag):
+        wn_tag = penn_to_wn(tag)
+        if wn_tag is None:
+            return None
+
+        try:
+            return wn.synsets(word, wn_tag)[0]
+        except:
+            return None
+
+    def sentence_similarity(sentence1, sentence2):
+        """ compute the sentence similarity using Wordnet """
+        # Tokenize and tag
+        sentence1 = pos_tag(word_tokenize(sentence1))
+        sentence2 = pos_tag(word_tokenize(sentence2))
+
+        # Get the synsets for the tagged words
+        synsets1 = [tagged_to_synset(*tagged_word) for tagged_word in sentence1]
+        synsets2 = [tagged_to_synset(*tagged_word) for tagged_word in sentence2]
+
+        # Filter out the Nones
+        synsets1 = [ss for ss in synsets1 if ss]
+        synsets2 = [ss for ss in synsets2 if ss]
+
+        score, count = 0.0, 0
+
+        # For each word in the first sentence
+        for synset in synsets1:
+            # Get the similarity value of the most similar word in the other sentence
+            lst = [synset.path_similarity(ss) for ss in synsets2]
+            if lst:
+                best_score = max(lst)
+            else:
+                best_score = 2
+
+            # Check that the similarity could have been computed
+            if best_score is not None:
+                score += best_score
+                count += 1
+
+        # Average the values
+        if count!=0:
+            score /= count
+            return score
+        else:
+            return 0
+
+    sentence = s1
+
+    focus_sentence = s2
+
+    def symmetric_sentence_similarity(sentence1, sentence2):
+        """ compute the symmetric sentence similarity using Wordnet """
+        return (sentence_similarity(sentence1, sentence2) + sentence_similarity(sentence2, sentence1)) / 2
+
+    return symmetric_sentence_similarity(focus_sentence, sentence)
+
+def most_sim(sentence):
+  return words[np.argmax(np.dot(entire.toarray(),tf.transform([sentence]).toarray().T))]
+
+tf = TfidfVectorizer()
+entire = tf.fit_transform(words)
+
+disorder = raw_input('enter disorder: ')
+ms = most_sim(disorder)
+if similarity(disorder,ms) > .5:
+    print ms
+else:
+    print 'not enough data'
